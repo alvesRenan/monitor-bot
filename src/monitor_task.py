b1a28257f75b1f391e1f93826fd1f41328b81992
Renan Alves <barbosa.alves.renan@gmail.com>
2023-01-15 16:09:46 -0300

    Updated monitor task

--- src/monitor_task.py
+++ src/monitor_task.py
@@ -8,18 +8,24 @@ STATUS = 'Running'
 def check_status(app_name):
     pods = CLI.list_pod_for_all_namespaces(watch=False)
 
-    for pod in pods:
+    for pod in pods.items:
         if pod.metadata.labels.get('app') == app_name:
             return pod.status.phase
+    
+    return None
 
 def monitor_task(mercurio, id, app_name):
     NEW_STATUS = check_status(app_name)
+
+    if STATUS == None:
+        monitor_status_text = f'Aplicação não encontrada'
+        mercurio.send_message(id, monitor_status_text)
 
     if NEW_STATUS != STATUS:
         monitor_status_text = f'O estado da aplicação mudou, o atual estado é: {NEW_STATUS}'
         mercurio.send_message(id, monitor_status_text)
 
-        STATUS = NEW_STATUS
+    STATUS = NEW_STATUS
     
     sleep(5)
 
