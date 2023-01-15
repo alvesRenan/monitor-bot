93ad37d5728a4966a5d821d22c99a0fc741b5858
Renan Alves <barbosa.alves.renan@gmail.com>
2023-01-15 15:48:56 -0300

    Update deploy script and fix yaml file

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -6,7 +6,8 @@
 
 docker build . -t renanalves/bot-monitor
 
-docker push -u renanalves -p $1 renanalves/test-app
+docker login -u renanalves -p "${1}"
+docker push renanalves/bot-monitor
 
 sed -i 's@KEY_VALUE@'"${2}"'@' k8s-files/deployment.yaml
 kubectl apply -f k8s-files
