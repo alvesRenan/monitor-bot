eb00271981abdbc18bed8d9733a816baf22de3ac
Renan Alves <barbosa.alves.renan@gmail.com>
2023-01-15 16:49:43 -0300

    Ajust log level

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,11 @@ import logging
 
 class Log:
     def __init__(self) -> None:
-        logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
+        logging.basicConfig(
+            format='%(asctime)s %(message)s', 
+            datefmt='%d/%m/%Y %I:%M:%S %p',
+            level=logging.INFO
+        )
     
     @staticmethod
     def log(msg):
